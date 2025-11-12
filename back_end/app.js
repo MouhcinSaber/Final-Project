ef22e525f51f420189ea5cd59976d7ccdf28dabc
Mouhcin Saber <m.benyammi.16@gmail.com>
2025-11-12 17:38:05 +0100

    sending Auth

--- back_end/app.js
+++ back_end/app.js
@@ -19,12 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 // Define routes
 const chatRoutes = require('./routes/chats.js');
 const fieldOfStudyRoutes = require('./routes/field_of_study.js');
-
+const authentificationRoutes = require('./routes/Authentification.js');
 
 app.use('/api/chats', chatRoutes);
 app.use('/api/fields_of_study', fieldOfStudyRoutes);
-
-
+app.use('/api/authentification', authentificationRoutes);
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
