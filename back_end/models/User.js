07b6bed81c5820461853973d3e2aeceabad3014f
Mouhcin Saber <m.benyammi.16@gmail.com>
2025-11-12 16:13:24 +0100

    uploading user

---
+++ back_end/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const UserProfileSchema = new mongoose.Schema({
+/*  _Id
+b.	Password 
+c.	Field of study
+d.	University name
+e.	Username
+f.	email
+g.	Gender
+h.	Profile picture
+i.	Conversations*/
+    _id : {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+    },
+    Password : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email : {
+        type: String,
+        required: true,
+    },
+    Username : {
+        type: String,
+        required: true,
+    },
+    Field_of_study : {
+        type: String,
+        required: true,
+    },
+    University_name : {
+        type: String,
+        required: true,
+    },
+    Gender : {
+        type: String,
+        required: true,
+    },
+    Profile_picture : {
+        type: String,
+        required: false,
+    },
+    Conversations: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ConversationSchema'
+    }]
+}, { timestamps: true });
+module.exports = mongoose.model('UserProfile', UserProfileSchema);
